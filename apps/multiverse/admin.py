a3aee8d9533f2cbe80c06cf4bd62fedf73e83ddf
Héctor Torres <hector.torres@simpat.tech>
2026-02-26 14:26:06 -0600

    Profile Views & URLS

--- apps/multiverse/admin.py
+++ apps/multiverse/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Profile
 
 # Register your models here.
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profiles'
+    max_num = 1
+    
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInLine,)
+    list_display = ('username', 'email', 'get_rank', 'is_staff')
+    
+    def get_rank(self, obj):
+        return obj.profile.get_rank_display()
+    get_rank.short_description = 'Rank'
+
+
+# Registry
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- apps/multiverse/views.py
+++ apps/multiverse/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from typing import Any
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView, UpdateView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .models import Profile
 
 # Create your views here.
+class LandingPageView(TemplateView):
+    template_name = 'multiverse/landing.html'
+    
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = 'multiverse/dashboard.html'
+    
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        profile = self.request.user.profile
+        context['profile'] = profile
+        return context
+        
+        
+class ProfileDetailView(LoginRequiredMixin, DetailView):
+    model = Profile
+    template_name = 'multiverse/profile_view.html'
+    context_object_name = 'user_profile'
+    
+    def get_object(self):
+        pk = self.kwargs.get('pk')
+        if not pk:
+            return self.request.user.profile
+        return get_object_or_404(Profile, pk=pk)
+    
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = Profile
+    template_name = 'multiverse/profile_update.html'
+    fields = [
+        'bio', 'location', 'avatar_url', 
+        'twitch', 'instagram', 'tiktok', 'facebook'
+    ]
+    success_url = reverse_lazy('multiverse:profile_self')
+    
+    def get_object(self):
+        return self.request.user.profile
+    
+    
